e35c6ccd767d66f196c9bd6a0b06d136c2676c6b
hmjlon <jiyoung110077@gmail.com>
2025-02-12 09:45:48 +0900

    first commit

---
+++ js/3.js
@@ -0,0 +1,25 @@
+$(document).ready(function(){
+  $("body").dblclick(function(){
+    alert("Double click")
+  })
+  $("#btn_1").click(function(){
+    $("p").hide()
+  })
+
+  $("p").mouseenter(function(){
+    $(this).css("background-color", "gold")
+  })
+
+  $("p").mouseleave(function(){
+    $(this).css("background-color", "white")
+    
+  })
+  $("p").mousedown(function(){
+    $(this).css("background-color", "pink")
+    
+  })
+  $("p").mouseup(function(){
+    $(this).css("background-color", "grey")
+    
+  })
+})
---
+++ js/4.js
@@ -0,0 +1,31 @@
+$(document).ready(function(){
+  $(".outer").mouseover(function(){
+    console.log("Outer Mouse Over Event")
+  })
+
+  $(".outer").mouseenter(function(){
+    console.log("Outer Mouse Enter Event")
+  })
+
+  $(".inner").mouseover(function(){
+    console.log("Inner Mouse Over Event")
+  })
+
+  $(".inner").mouseenter(function(){
+    console.log("Inner Mouse Enter Event")
+  })
+
+
+  //  input 상자 선택되면 핑크색
+  // 선택 해제되면 노랑색 
+
+  $("input[type='password']").focus(function(){
+    $(this).css("background-color", "pink")
+    
+  })
+
+  $("input[type='password']").blur(function(){
+    $(this).css("background-color", "yellow")
+    
+  })
+})
