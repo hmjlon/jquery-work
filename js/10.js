23e43cbcabab227b1df88c3a7ebc5f1398fe00d9
hmjlon <jiyoung110077@gmail.com>
2025-02-13 10:42:42 +0900

    forLoop

--- js/10.js
+++ js/10.js
@@ -1,12 +1,26 @@
 $(document).ready(function(){
   let data = [
-    {name : '장원영' , age : 21 },
-    {name : '안유진' , age : 22 },
-    {name : '리즈' , age : 21 },
-    {name : '레이' , age : 22 },
-    {name : '이서' , age : 18 },
-    {name : '가을' , age : 23 },
+    { name: "장원영", age: 21 },
+    { name: "안유진", age: 22 },
+    { name: "리즈", age: 21 },
+    { name: "레이", age: 22 },
+    { name: "이서", age: 18 },
+    { name: "가을", age: 23 },
+  ];
 
-  ]
-
+  $("#forLoop").on("click", function () {
+    let result = "";
+    for (let i = 0; i < data.length; i++) {
+      result =
+        result +
+        `
+        <tr>
+          <td>${i + 1}</td>
+          <td>${data[i].name}</td>
+          <td>${data[i].age}</td>
+        </tr>
+  `;
+    }
+    $("#forOut").html(result);
+  });
 })
